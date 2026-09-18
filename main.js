bf71c2a30b4d46c0f51ca166aa5be345caaab124
invitacionesdigitales <111189010+invitacionesdigitales@users.noreply.github.com>
2026-09-18 10:29:21 -0300

    final

--- main.js
+++ main.js
@@ -73,49 +73,22 @@ function diaEventoAhora () {
 
 
 
-
-
-  boton.addEventListener("click", () => {
+   boton.addEventListener("click", () => {
 
     Swal.fire({
       title: '<strong>VER LA UBICACION<u></u></strong>',
       icon: 'success',
       html:
         'INGRESÁ' +
-        '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3340.265314609566!2d-62.85271132459842!3d-33.15466088242459!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95ceca955d0023fd%3A0x376ebd85ba548d6a!2sDoctor%20Dario%20Galassi%20124%2C%20X2657%20Laborde%2C%20C%C3%B3rdoba!5e0!3m2!1ses!2sar!4v1783375951028!5m2!1ses!2sar" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="strict-origin-when-cross-origin"></iframe>',
+        '<iframe src="https://www.google.com/maps/embed?pb=!4v1778935429563!6m8!1m7!1sjGE5thmhTUgxrET0_2Ru7A!2m2!1d-33.1540884760236!2d-62.86196557590743!3f294.8475504045752!4f-2.854366487984109!5f0.7820865974627469" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>',
         
         
     })
   });
    
    
-  function exito(){
-    Swal.fire({
-        title: '<strong>VER LA UBICACION<u></u></strong>',
-        icon: 'success',
-        html:
-          'INGRESÁ' +
-          '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3146.02361420434!2d-57.7721125!3d-37.9532346!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95852f05a374a15f%3A0x61475146b9311c2e!2sParroquia%20Nuestra%20Se%C3%B1ora%20del%20Pilar!5e0!3m2!1ses!2sar!4v1758125223088!5m2!1ses!2sar" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>' +
-          'Podes ampliar el mapa',
-          
-          
-      })
-}
-   
-      
-  // Botón 2 (nuevo)
-  const boton2 = document.getElementById("boton2");
-  boton2.addEventListener("click", () => {
-    Swal.fire({
-      title: '<strong>VER LA UBICACION</strong>',
-      icon: 'success',
-      html:
-        'INGRESÁ' +
-        '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3147.0646111938745!2d-57.761958!3d-37.928920999999995!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95852efde2df4f1f%3A0x9a2af04341877482!2sLa%20Victoriana!5e0!3m2!1ses!2sar!4v1758125963480!5m2!1ses!2sar" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>'
-    })
-  });
-  
 
+    
     
     
 
